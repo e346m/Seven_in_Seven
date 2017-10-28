12056bf678346516a6985bcc16b83b7d90d8fd1f
eiji mishiro <eiji346g@gmail.com>
2017-10-28 11:00:12 +0900

    done day 1

--- Concurrency/Chapter_5_Elixir/day1/hello_actors.exs
+++ Concurrency/Chapter_5_Elixir/day1/hello_actors.exs
@@ -4,13 +4,19 @@ defmodule Talker do
       {:greet, name} -> IO.puts("Hello #{name}")
       {:praise, name} -> IO.puts("#{name}, you're amazing")
       {:celebrate, name, age} -> IO.puts("Here's to another #{age} yours, #{name}")
+      {:shutdown} -> exit(:normal)
     end
     loop
   end
 end
 
-pid = spawn(&Talker.loop/0)
+Process.flag(:trap_exit, true)
+pid = spawn_link(&Talker.loop/0)
 send pid, {:greet, "Eiji"}
 send pid, {:praise, "Mishiro"}
 send pid, {:celebrate, "Eiji Mishiro", 25}
-:timer.sleep(1000)
+send pid, {:shutdown}
+
+receive do
+  {:EXIT, ^pid, reason} -> IO.puts("Talker has exited (#{reason})")
+end
